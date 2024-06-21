a313e88ed6682bef7c9bd5cd214425afd0ff2c5f
Aayush Pradhan <aayushpradhan488@gmail.com>
2024-06-21 10:00:08 +0530

    NamePage added

--- example/views.py
+++ example/views.py
@@ -2,15 +2,9 @@
 from datetime import datetime
 
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    namepage = loader.get_template('name.html')
+    return HttpResponse(namepage.render())
